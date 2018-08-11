d0487501f32e1ac9aa90920bc4e85fcf81338aab
Felipe Martins da Silva <lipe.vsr@gmail.com>
2018-08-11 15:09:55 -0300

    Adicionado ngStyle

--- Directives/src/app/app.module.ts
+++ Directives/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { NgIfComponent } from './ng-if/ng-if.component';
 import { NgSwitchComponent } from './ng-switch/ng-switch.component';
 import { NgForComponent } from './ng-for/ng-for.component';
 import { NgClassComponent } from './ng-class/ng-class.component';
+import { NgStyleComponent } from './ng-style/ng-style.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { NgClassComponent } from './ng-class/ng-class.component';
     NgIfComponent,
     NgSwitchComponent,
     NgForComponent,
-    NgClassComponent
+    NgClassComponent,
+    NgStyleComponent
   ],
   imports: [
     BrowserModule
