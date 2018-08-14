944219b39af4f55091085e82282e420d9d00e45d
Felipe Martins da Silva <lipe.vsr@gmail.com>
2018-08-14 12:07:25 -0300

    Adicionado componente com operador elvis

--- Directives/src/app/app.module.ts
+++ Directives/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { NgSwitchComponent } from './ng-switch/ng-switch.component';
 import { NgForComponent } from './ng-for/ng-for.component';
 import { NgClassComponent } from './ng-class/ng-class.component';
 import { NgStyleComponent } from './ng-style/ng-style.component';
+import { ElvisComponent } from './elvis/elvis.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { NgStyleComponent } from './ng-style/ng-style.component';
     NgSwitchComponent,
     NgForComponent,
     NgClassComponent,
-    NgStyleComponent
+    NgStyleComponent,
+    ElvisComponent
   ],
   imports: [
     BrowserModule,
